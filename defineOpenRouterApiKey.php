d2d450ff3000b78fb995fc8effff8b51e5ec6512
Shawn Altman <shawn@dreamspectrum.com>
2025-06-21 12:23:56 -0700

    Hardcode default OpenRouter API key

--- defineOpenRouterApiKey.php
+++ defineOpenRouterApiKey.php
@@ -3,5 +3,7 @@ if (!defined('OPENROUTER_API_KEY')) {
     $envKey = getenv('OPENROUTER_API_KEY');
     if ($envKey !== false && $envKey !== '') {
         define('OPENROUTER_API_KEY', $envKey);
+    } else {
+        define('OPENROUTER_API_KEY', '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');
     }
 }
